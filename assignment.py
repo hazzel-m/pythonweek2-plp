9144a4aee52d311c1af7c18b80fa7388aa4fb62e
hazzel-m <mochogehazzel@gmail.com>
2025-03-20 00:09:26 +0300

    week 2 assignment done

---
+++ assignment.py
@@ -0,0 +1,34 @@
+# Lists
+# creating an emoty list
+my_list = []
+print(my_list)
+
+# appending values to my_list
+list = [10,20,30,40]
+my_list.append(list)
+print(my_list)
+
+# Inserting a value to my_list
+my_list[0].insert(1,15)
+print(my_list)
+
+# Extending the list
+my_list[0].extend([50,60,70])
+print(my_list)
+
+# Removing the last value from my_list
+my_list[0].pop()
+print(my_list)
+
+# Sorting in ascending order
+my_list[0].sort()
+print(my_list)
+
+# finding and printing the index of 30
+print(my_list[0].index(30))
+
+
+
+
+
+
